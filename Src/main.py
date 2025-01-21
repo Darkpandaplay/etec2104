e59bd2c4c73d389ae3abb5c75163d940fe62f5bc
Darkpandaplay <52468163+Darkpandaplay@users.noreply.github.com>
2025-01-21 15:07:47 -0500

    Currently broken code

--- Src/main.py
+++ Src/main.py
@@ -7,12 +7,18 @@ PYPATH = os.path.dirname(__file__)
 class App:
     @cherrypy.expose
     def index(self):
-        return "I like pie!"
+        s = mako.template.Template(filename=f"{PYPATH}/../html/home.html")
+        return s.render()
 
     @cherrypy.expose
     def test(self):
         t = mako.template.Template(filename=f"{PYPATH}/../html/test.html")
         return t.render(foobar=42)
+    
+    @cherrypy.expose
+    def posts(self):
+        l = mako.template.Template(filename=f"{PYPATH}/../html/posts.html")
+        return l.render(foobar=100)
 
 app = App()
 cherrypy.quickstart(app)
